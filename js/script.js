560b7d74baf52fefb909caa9ba707afde8797e81
betosantanna <betosantanna@outlook.com>
2020-07-19 13:25:02 -0300

    Script
    Added the script to get the button and the elements

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var btnAdd = document.querySelector(".btn-add-task");
+var btnSave = document.querySelector("btn-save");
+var btnCancel = document.querySelector(".btn-cancel");
+var btnMenu = document.querySelector(".btn-menu");
+
+var taskHolder = document.querySelector(".task-holder");
+var task = document.querySelector(".task");
+var taskChecker = document.querySelector("task-checker");
+var creatorWrapper = document.querySelector(".creator-wrapper");
