283ab6e1c8e68d3ba89436e0b417bb1039c7795e
betosantanna <betosantanna@outlook.com>
2020-07-23 15:10:17 -0300

    Created a function to add the text

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ var btnAdd = document.querySelector(".btn-add-task");
 var btnSave = document.querySelector(".btn-save");
 var btnCancel = document.querySelector(".btn-cancel");
 var btnMenu = document.querySelector(".btn-menu");
-
 var taskHolder = document.querySelector(".task-holder");
 var creatorWrapper = document.querySelector(".creator-wrapper");
 var userInput = document.querySelector(".user-input");
-
+var task;
+var taskChecker;
 var tasks = [];
 
 function verifyIfEmpty() {
@@ -18,32 +18,39 @@ function verifyIfEmpty() {
 }
 
 function createElements() {
-  var taskChecker = document.createElement("input");
-  var task = document.createElement("li");
-  var taskText = document.createTextNode(userInput.value);
-
+  taskChecker = document.createElement("input");
+  task = document.createElement("li");
   taskChecker.classList.add("task-checker");
   task.classList.add("task");
-
   taskChecker.setAttribute("type", "checkbox");
   task.appendChild(taskChecker);
-  task.appendChild(taskText);
-  userInput.value = "";
-  userInput.focus();
-  taskHolder.appendChild(task);
-  tasks.push(taskText);
+
 }
 
-function cancel() {
-  btnCancel.onclick = userInput.value = "";
+function addText() {
+  var taskText = document.createTextNode(userInput.value);
+  task.appendChild(taskText);
+  taskHolder.appendChild(task);
+  tasks.push(task);
+  userInput.value = "";
+  userInput.focus();
+}
+
+function saveOnStorage() {
+  localStorage.setItem("allTasks", JSON.stringify(tasks));
 }
 
 btnSave.onclick = () => {
   verifyIfEmpty();
+  createElements();
+  addText();
+  saveOnStorage();
 }
-userInput.onkeypress = () => {
-  if (userInput.value > 0 && e.keyCode === 13) {
+userInput.onkeypress = (e) => {
+  if (userInput.value.length > 0 && e.keyCode === 13) {
+    verifyIfEmpty();
     createElements();
+    addText();
+    saveOnStorage();
   }
-
 }
